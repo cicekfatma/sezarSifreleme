c8841a3a34505f5e268105c174a1dcb1237642b5
Fatma Cicek <fatmacicek139@gmail.com>
2025-07-22 14:32:31 +0300

    sezar algoritması uygulandı

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,8 @@ import { Routes } from '@angular/router';
 import { Sezar } from './sezar/sezar';
 import { RouterModule } from '@angular/router';
 export const routes: Routes = [
+
+  { path: '', redirectTo: 'sezar', pathMatch: 'full' },
   { path: 'sezar',
      component: Sezar
     }
--- src/app/sezar/sezar.ts
+++ src/app/sezar/sezar.ts
@@ -1,12 +1,49 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-sezar',
+  standalone: true,
+  imports: [FormsModule],
   templateUrl: './sezar.html',
   styleUrls: ['./sezar.css']
 })
 export class Sezar {
+      text: string = '';
+      key: number = 3; // Default key value
+      result: string = '';
 
+      alphabet: string[]= ['a','b','c','ç','d','e','f','g','ğ','h','ı','i','j','k','l','m','n','o','ö','p','r','s','ş','t','u','ü','v','y', 'z'];
 
+      encrypt() {
+        this.result= this.processText(this.text.toLowerCase(),this.key,true);
+      }
+      decrypt(){
+        this.result= this.processText(this.text.toLowerCase(),this.key,false);
+      }
+      private processText(text: string, key: number, isEncrypt: boolean): string {
+
+        let output = '';
+        const alphabetLength = this.alphabet.length;
+
+        for(let i = 0; i < text.length; i++) {
+          const char = text[i];
+          const index = this.alphabet.indexOf(char);
+
+          if(index=== -1){
+            output += char;
+          }
+          else{
+            const newIndex = isEncrypt
+            ? (index + key) % alphabetLength
+            : (index - key + alphabetLength) % alphabetLength;
+
+            output += this.alphabet[newIndex];
+          }
+        }
+
+        return output;
+      }
 }
